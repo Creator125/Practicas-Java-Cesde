836bd9756d1678d02ca7a5dc2729f1f2a09328a0
Creator125 <85368990+Creator125@users.noreply.github.com>
2022-08-28 14:06:13 -0500

    Modulo1: Fumigando bug en ejercicio9.java

--- Modulo1/src/modulo1/ejercicio9.java
+++ Modulo1/src/modulo1/ejercicio9.java
@@ -9,10 +9,17 @@ package modulo1;
 
 public class ejercicio9 {
     public static void main(String[] args) {
-        boolean a,b;
+        boolean a,b,o;
         a = 9 * (4 - 7 / 5) % 3 > 8 / (4 - 9);
-        b = 10 + (24 - 31)^2 > (3 * 4 + 9 - 3 ) < (2 - 1);
         
+        //Resolviendo "b. 10+ (24 – 31) ^ 2 > (3 * 4 + 9 - 3 ) < (2 – 1)"
+        //JAVA NO PUEDE COMPARAR BOOLEAN CON INT
+        o = (10 + (24 - 31)^2) > (3 * 4 + 9 - 3 );
+        int n;
+        n = o ? 1 : 0; //Covirtiendo en boolean a int
+        b = n < (2 - 1);
+        
+        //Resultados
         System.out.print("a. "+ a +"\n"); //item a
         System.out.print("b. "+ b +"\n"); //item b
     }
