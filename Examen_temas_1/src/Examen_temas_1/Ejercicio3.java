60819eae1b17b3c55c7db80ebf0ec00ad4c27ae3
Creator125 <85368990+Creator125@users.noreply.github.com>
2022-10-21 19:13:29 -0500

    Sesión 9: Conclusión de ejercicios

---
+++ Examen_temas_1/src/Examen_temas_1/Ejercicio3.java
@@ -0,0 +1,10 @@
+/*
+Desarrolle un programa escrito en lenguaje JAVA en el que se solicite un número 
+al usuario entre 1 y 7 y que muestre a qué día de la semana corresponde siendo 
+1 Lunes y 7 Domingo. Realice este programa utilizando la estructura SWITCH CASE.
+ */
+package Examen_temas_1;
+
+public class Ejercicio3 {
+    
+}
