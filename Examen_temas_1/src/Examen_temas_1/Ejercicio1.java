8254d38d9f26cb1403f6d375dfd21d4916c31265
Creator125 <85368990+Creator125@users.noreply.github.com>
2023-05-09 04:38:10 -0500

    Reparando problemas

--- Examen_temas_1/src/Examen_temas_1/Ejercicio1.java
+++ Examen_temas_1/src/Examen_temas_1/Ejercicio1.java
@@ -3,22 +3,23 @@ Desarrolle un programa escrito en lenguaje JAVA en el que se solicite el peso y
 la estatura de una persona e indique la clasificación de su peso de acuerdo al 
 IMC (Indice de Masa Corporal).
  */
-package Examen_temas_1;
+package examen_temas_1;
 import java.util.Scanner;
 import java.math.*;
+import java.util.Locale;
 
 public class Ejercicio1 {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         double peso, estatura, resultado;
         
         //Entrada para ingresar el 'peso'
         System.out.println("Ingrese el peso");
-        Scanner dato = new Scanner(System.in);
+        Scanner dato = new Scanner(System.in).useLocale(Locale.US);
         peso = dato.nextDouble();
         
         //Entrada para ingresar la 'estatura'
         System.out.println("Ingrese la estatura");
-        Scanner dato2 = new Scanner(System.in);
+        Scanner dato2 = new Scanner(System.in).useLocale(Locale.US);
         estatura = dato2.nextDouble();
         
         resultado = peso / Math.sqrt(estatura); //Hallando el IMC
--- Examen_temas_1/src/Examen_temas_1/Ejercicio3.java
+++ Examen_temas_1/src/Examen_temas_1/Ejercicio3.java
@@ -2,9 +2,49 @@
 Desarrolle un programa escrito en lenguaje JAVA en el que se solicite un número 
 al usuario entre 1 y 7 y que muestre a qué día de la semana corresponde siendo 
 1 Lunes y 7 Domingo. Realice este programa utilizando la estructura SWITCH CASE.
- */
+*/
 package Examen_temas_1;
+import java.util.Scanner;
 
 public class Ejercicio3 {
-    
+    public static void main(String[] args) {
+        int dia;
+        
+        System.out.println("Ingrese un numero entre 1 y 7");
+        Scanner dato = new Scanner(System.in);
+        dia = dato.nextInt();
+        
+        switch(dia){
+            case 1:
+                System.out.println("Lunes");
+            break;
+            
+            case 2:
+                System.out.println("Martes");
+            break;
+            
+            case 3:
+                System.out.println("Miercoles");
+            break;
+            
+            case 4:
+                System.out.println("Jueves");
+            break;
+            
+            case 5:
+                System.out.println("Viernes");
+            break;
+            
+            case 6:
+                System.out.println("Sabado");
+            break;
+            
+            case 7:
+                System.out.println("Domingo");
+            break;
+            
+            default:
+                System.out.println("El día " + dia + " no existe, debe se un dia de la semana");
+        }
+    }
 }
